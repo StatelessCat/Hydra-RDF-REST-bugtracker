837afa8a4aae93d30070b3ecda0c59ce752ce9d1
Raphael Cazenave-Leveque <raphael.cazenave@gmail.com>
2015-02-05 23:41:53 +0100

    a working example of usage of RDF-REST-JS Json-LD parser on an AngularJS app (need to start the server.js, and open a browser on localhost:8080/public to watch the example). rdfrestbundle.js is generated using browserify tool.

--- public/js/ng-index.js
+++ public/js/ng-index.js
@@ -1,11 +1,46 @@
+function UserController($scope, $http, $window){
+    $scope.users = [{}];
 
-function UserController($scope, $http){
-    $scope.users = [];
+    var parser = parserFactory.getParser({
+        contentType:'application/ld+json',
+        graph: graph.graph()
+    });
 
-    $http.get('http://localhost:8080/api/user')
-        .success(function(data, status, headers, config) {
-            $scope.users = data;
-        })
-        .error(function(data, status, headers, config) {
+    var doc = {
+        "@context": "http://json-ld.org/contexts/person.jsonld",
+        "@id": "http://dbpedia.org/resource/John_Lennon",
+        "name": "John Lennon",
+        "born": "1940-10-09",
+        "spouse": "http://dbpedia.org/resource/Cynthia_Lennon"
+    };
+
+    parser.addChunk(JSON.stringify(doc));
+
+    var aPromise = parser.finalize();
+
+    aPromise.then(function (graph) {
+        graph.forEachTriple(null, null, null, console.info.bind(console));
+        graph.forEachTriple(null, null, null, function(s, p, o) {
+            if (! $scope.users[0]) {
+                $scope.users[0] = {};
+                $scope.$apply(); // ugly
+            } else {
+                if ((o['@type']) && (o['@type'].match("^.*string.*$"))) {
+                    $scope.users[0][p['@id']] = o['@value'];
+                } else {
+                    $scope.users[0][p['@id']] = o['@id'];
+                }
+
+                $scope.$apply(); // ugly
+            }
         });
+    });
+
+    $scope.message = "Waiting 2000ms for update";
+
+    setTimeout(function () {
+        $scope.$apply(function () {
+            $scope.message = "Timeout called!";
+        });
+    }, 2000);
 }
