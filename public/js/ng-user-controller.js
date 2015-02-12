badc452fec625bd9728cf206ed21666941571382
Raphael Cazenave-Leveque <raphael.cazenave@gmail.com>
2015-02-12 01:14:22 +0100

    almost working

--- public/js/ng-user-controller.js
+++ public/js/ng-user-controller.js
@@ -16,54 +16,13 @@ function UserController($scope) {
 
             var ressource = coreFactory.getCore("http://localhost:8080/api/user/54da8d9d0f7c387641739640");
             ressource.edit(function(graph) {
-                return graph.addTriple($scope.newSubject, $scope.newPredicate, $scope.newObject)
-                    .then(function() {
-                        var ressource2 = coreFactory.getCore("http://localhost:8080/api/user/54da8d9d0f7c387641739640");
-
-                        ressource2.getState()
-                            .then(function (g) {
-                                // Here we have an in-memory graph representation
-                                var strJson = "";
-
-                                // Creating an instance of the serialiser from the factory
-                                var serialiser = serializerFactory.getSerializer({
-                                    contentType: "application/ld+json",
-                                    graph: g
-                                });
-
-                                // Serialising the Graph to JSON-LD
-                                return serialiser(function (line) {
-                                    strJson += line; // Chunks of JSON-LD here !
-                                }).then(function () {
-
-                                    console.log("JSONLD REPRESENTATION:");
-
-                                    // strJson contains the full JSON-LD
-                                    // we hope it's the same as the original one
-                                    console.log(strJson);
-                                    angular.fromJson(strJson).forEach(function (e) {
-                                        var json = {};
-                                        json.subject = e["@id"];
-                                        for (var key in e) {
-                                            if (e.hasOwnProperty(key) && key !== "@id") {
-                                                json.predicate = key;
-                                                if (e[key]["@value"]) {
-                                                    json.object = e[key]["@value"];
-                                                } else {
-                                                    json.object = e[key]["@id"];
-                                                }
-                                            }
-                                        }
-                                        $scope.triples.push(json);
-                                    });
-
-                                    $scope.$apply(); // ugly
-
-                                });
-                            });
-                    }).catch(function(reason) {
-                        console.log(reason);
-                    });
+                var ns = rdfNode.namespace("http://ex.co/vocab#");
+                return graph.addTriple( rdfNode.iri('http://champin.net/#pa'), ns($scope.newPredicate), ns($scope.newObject));
+            }).then(function(g) {
+                console.log("----\n");
+                return serializerNTriples.nt(g, console.log);
+            }).catch(function(reason){
+                console.log(reason);
             });
         }
     };
